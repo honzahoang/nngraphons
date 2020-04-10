c5a98d3a35b3f98d90adef787ddb370fa4005b00
honzahoang <36709203+honzahoang@users.noreply.github.com>
2020-04-10 19:40:41 +0200

    Create nngraphons package

--- nngraphons/graphon_gradient_learning.py
+++ nngraphons/graphon_gradient_learning.py
@@ -83,42 +83,52 @@ def gaussian(alpha):
     phi = torch.exp(-1*alpha.pow(2))
     return phi
 
+
 def linear(alpha):
     phi = alpha
     return phi
+
 
 def quadratic(alpha):
     phi = alpha.pow(2)
     return phi
 
+
 def inverse_quadratic(alpha):
     phi = torch.ones_like(alpha) / (torch.ones_like(alpha) + alpha.pow(2))
     return phi
+
 
 def multiquadric(alpha):
     phi = (torch.ones_like(alpha) + alpha.pow(2)).pow(0.5)
     return phi
 
+
 def inverse_multiquadric(alpha):
     phi = torch.ones_like(alpha) / (torch.ones_like(alpha) + alpha.pow(2)).pow(0.5)
     return phi
+
 
 def spline(alpha):
     phi = (alpha.pow(2) * torch.log(alpha + torch.ones_like(alpha)))
     return phi
 
+
 def poisson_one(alpha):
     phi = (alpha - torch.ones_like(alpha)) * torch.exp(-alpha)
     return phi
+
 
 def poisson_two(alpha):
     phi = ((alpha - 2*torch.ones_like(alpha)) / 2*torch.ones_like(alpha)) \
     * alpha * torch.exp(-alpha)
     return phi
 
+
 def matern32(alpha):
     phi = (torch.ones_like(alpha) + 3**0.5*alpha)*torch.exp(-3**0.5*alpha)
     return phi
+
 
 def matern52(alpha):
     phi = (torch.ones_like(alpha) + 5**0.5*alpha + (5/3) \
