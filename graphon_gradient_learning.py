41379a4c23797272a31fb423f548b3d6e45bd41b
honzahoang <36709203+honzahoang@users.noreply.github.com>
2020-04-10 15:57:21 +0200

    April update

--- graphon_gradient_learning.py
+++ graphon_gradient_learning.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import matplotlib.pyplot as plt
 from typing import List
 
 
@@ -18,15 +19,149 @@ class MLP(nn.Module):
         # Output layer
         self.output_layer = nn.Linear(self.hidden_size, 1)
 
+        # Randomly initialize weights
+        nn.init.xavier_uniform_(self.input_layer.weight, gain=2)
+        nn.init.xavier_uniform_(self.hidden1.weight, gain=2)
+        nn.init.xavier_uniform_(self.hidden2.weight, gain=2)
+        nn.init.xavier_uniform_(self.output_layer.weight, gain=2)
+
     def forward(self, x):
         x = self.input_layer(x)
         x = F.relu(self.hidden1(x))
         x = F.relu(self.hidden2(x))
-        x = self.output_layer(x)
+        x = torch.sigmoid(self.output_layer(x))
         return x
 
 
-def backprop(net: torch.nn.Module, g: List, t_g: List[float], n: int):
+# https://github.com/JeremyLinux/PyTorch-Radial-Basis-Function-Layer
+class RBFLayer(nn.Module):
+    """
+    Transforms incoming data using a given radial basis function:
+    u_{i} = rbf(||x - c_{i}|| / s_{i})
+    Arguments:
+        in_features: size of each input sample
+        out_features: size of each output sample
+    Shape:
+        - Input: (N, in_features) where N is an arbitrary batch size
+        - Output: (N, out_features) where N is an arbitrary batch size
+    Attributes:
+        centres: the learnable centres of shape (out_features, in_features).
+            The values are initialised from a standard normal distribution.
+            Normalising inputs to have mean 0 and standard deviation 1 is
+            recommended.
+
+        sigmas: the learnable scaling factors of shape (out_features).
+            The values are initialised as ones.
+
+        basis_func: the radial basis function used to transform the scaled
+            distances.
+    """
+
+    def __init__(self, in_features, out_features, basis_func):
+        super(RBFLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.centres = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.sigmas = nn.Parameter(torch.Tensor(out_features))
+        self.basis_func = basis_func
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.normal_(self.centres, 0, 1)
+        nn.init.constant_(self.sigmas, 1)
+
+    def forward(self, input):
+        size = (input.size(0), self.out_features, self.in_features)
+        x = input.unsqueeze(1).expand(size)
+        c = self.centres.unsqueeze(0).expand(size)
+        distances = (x - c).pow(2).sum(-1).pow(0.5) * self.sigmas.unsqueeze(0)
+        return self.basis_func(distances)
+
+
+# RBFs
+def gaussian(alpha):
+    phi = torch.exp(-1*alpha.pow(2))
+    return phi
+
+def linear(alpha):
+    phi = alpha
+    return phi
+
+def quadratic(alpha):
+    phi = alpha.pow(2)
+    return phi
+
+def inverse_quadratic(alpha):
+    phi = torch.ones_like(alpha) / (torch.ones_like(alpha) + alpha.pow(2))
+    return phi
+
+def multiquadric(alpha):
+    phi = (torch.ones_like(alpha) + alpha.pow(2)).pow(0.5)
+    return phi
+
+def inverse_multiquadric(alpha):
+    phi = torch.ones_like(alpha) / (torch.ones_like(alpha) + alpha.pow(2)).pow(0.5)
+    return phi
+
+def spline(alpha):
+    phi = (alpha.pow(2) * torch.log(alpha + torch.ones_like(alpha)))
+    return phi
+
+def poisson_one(alpha):
+    phi = (alpha - torch.ones_like(alpha)) * torch.exp(-alpha)
+    return phi
+
+def poisson_two(alpha):
+    phi = ((alpha - 2*torch.ones_like(alpha)) / 2*torch.ones_like(alpha)) \
+    * alpha * torch.exp(-alpha)
+    return phi
+
+def matern32(alpha):
+    phi = (torch.ones_like(alpha) + 3**0.5*alpha)*torch.exp(-3**0.5*alpha)
+    return phi
+
+def matern52(alpha):
+    phi = (torch.ones_like(alpha) + 5**0.5*alpha + (5/3) \
+    * alpha.pow(2))*torch.exp(-5**0.5*alpha)
+    return phi
+
+
+# class RBFNet(nn.Module):
+
+
+def t_nn(g, net, n, track_computation=True):
+    """Calculates the homomorphism density of finite graph g into the graphon net."""
+    torch.set_grad_enabled(track_computation)
+
+    # Unpack small graph structure
+    V, E = g
+
+    # Uniformly sample unit hyper-cube n times
+    S_n = np.random.random_sample(size=(n, len(V)))
+
+    # Map vertices to real numbers from <0,1> n times
+    mapped_edges = np.empty(shape=(len(E)*n, 2))
+    for j in range(n):
+        mapped_edges[j*len(E):(j+1)*len(E)] = S_n[j][E]
+
+    # Sort floats in each edge in ascending order to use upper triangle of graphon
+    mapped_edges.sort()
+    mapped_edges = torch.from_numpy(mapped_edges).float()
+
+    # Sample the network for different edge probabilites for the small graph g
+    net_output = net(mapped_edges)
+
+    # Calculate homomorphism density for the small graph g
+    reshaped_for_prod = net_output.view(len(E), n)
+    edge_probs_prods = torch.prod(reshaped_for_prod, dim=0)
+    hom_density = torch.sum(edge_probs_prods) / n
+
+    torch.set_grad_enabled(True)
+
+    return hom_density
+
+
+def backprop(net: torch.nn.Module, g: List, t_g: List[float], epsilon: float, gamma: float):
     """
     Parameters
     ----------
@@ -38,10 +173,12 @@ def backprop(net: torch.nn.Module, g: List, t_g: List[float], n: int):
     t_g : List
         List of homomorphism densities of small graphs g into the training
         graph, must be the same length as g
-    n : int
-        The number of times to sample the network for each small graph in g to
-        calculate the homomorphism density of that graph
+    epsilon : float
+    gamma : float
     """
+    # Sample size
+    n = math.ceil((math.log(2) - math.log(1 - gamma)) / (2*epsilon**2))
+
     # Create optimizer
     optimizer = optim.Adam(net.parameters())
 
@@ -52,38 +189,56 @@ def backprop(net: torch.nn.Module, g: List, t_g: List[float], n: int):
 
         errors = []
         for i in range(len(g)):
-            # Unpack small graph structure
-            V, E = g[i]
-            # Sample unit hyper-cube n times
-            S_n = np.random.random_sample(size=(n, len(V)))
-            # Map vertices to real numbers from <0,1>
-            mapped_edges = np.empty(shape=(len(E)*n, 2))
-            for i in range(n):
-                mapped_edges[i*len(E):(i+1)*len(E)] = S_n[i][E]
-            # Sort floats in each edge in ascending order to train the upper
-            # triangle of the graphon (network)
-            mapped_edges.sort()
-            mapped_edges = torch.from_numpy(mapped_edges)
-            # Sample the network for different edge probabilites for the small
-            # graph g[i]
-            net_output = net(mapped_edges)
-            # Calculate homomorphism density for the small graph g[i]
-            reshaped_for_prod = net_output.view(len(E), n)
-            edge_probs_prods = torch.prod(reshaped_for_prod, dim=1)
-            hom_density = torch.mean(edge_probs_prods)
             # Calculate square error of the homomorphism density w.r.t.
             # training graph homomorphism density for the graph g[i]
-            err = t_g[i] - hom_density
+            err = t_g[i] - t_nn(g[i], net, n, True)
             sqerr = err * err
             errors.append(sqerr)
         # Calculate final loss value
-        loss = torch.mean(torch.stack(errors))
+        loss = torch.max(torch.stack(errors))
 
         # Progress print
         print(f'Current loss: {loss.item()}')
+
+        # Visualize progress
+        visualize_pytorch_net_graphon(net)
+
+        # Stopping criterion
+        if loss.item() < 0.000001:
+            break
 
         # Backprop gradients
         loss.backward()
 
         # Update weights
         optimizer.step()
+
+
+def visualize_pytorch_net_graphon(net, resolution=300):
+    """Plots the neural network as a greyscale image. The darker the closer to 1."""
+    uniform_args = np.linspace(start=0, stop=1, num=resolution)
+    cartesian_product = np.transpose(
+        [np.tile(uniform_args, len(uniform_args)),
+         np.repeat(uniform_args, len(uniform_args))]
+    )
+    cartesian_product.sort()
+    with torch.no_grad():
+        img_mat = (
+            net(torch.from_numpy(cartesian_product).float())
+            .numpy()
+            .reshape(resolution, resolution)
+        )
+    # Make visualization symmetric
+    tril = np.tril_indices(len(img_mat))
+    img_mat[tril] = img_mat[tril[1],tril[0]]
+    plt.figure()
+    plt.imshow(
+        X=img_mat,
+        origin='lower',
+        extent=[0,1,0,1],
+        cmap='plasma',
+        vmin=0,
+        vmax=1
+    )
+    plt.colorbar()
+    plt.show()
--- synthetic_graphs.py
+++ synthetic_graphs.py
@@ -1,5 +1,8 @@
+from itertools import product
 import random
+import math
 import numpy as np
+import networkx as nx
 import matplotlib.pyplot as plt
 
 
@@ -26,6 +29,11 @@ def graphon_complete_bipartite(x, y):
     ).astype(float)
 
 
+def graphon_big_qlique(x, y):
+    """Complete bipartite graphon"""
+    return ((x <= 0.5) & (y <= 0.5)).astype(float)
+
+
 def sample_graphon(W, size=None, max_size=None):
     """Returns a random graph sampled from graphon W."""
     # Number of vertices
@@ -42,7 +50,7 @@ def sample_graphon(W, size=None, max_size=None):
     V = np.arange(len(V))
 
     # Threshold probabilites to decide which pairs become edges
-    E = np.random.uniform(size=len(edge_probs)) >= 0.5
+    E = np.random.uniform(size=len(edge_probs)) <= edge_probs
     E = edge_indices[E]
 
     return V, E
@@ -60,4 +68,134 @@ def visualize_graphon(W, resolution=300, ax=plt):
         .reshape(resolution, resolution)
     )
     plt.gray()
-    ax.imshow(X=1-img_mat, origin='lower', extent=[0, 1, 0, 1])
+    ax.imshow(
+        X=img_mat,
+        origin='lower',
+        extent=[0, 1, 0, 1],
+        cmap='plasma',
+        vmin=0,
+        vmax=1
+    )
+    ax.colorbar()
+
+
+def t_discrete(g, G, epsilon=0.01, gamma=0.95):
+    """Homomorphism density for finite graphs estimation with naive Monte-Carlo"""
+    # Unpack graph structs
+    V_g, E_g = g
+    V_G, E_G = G
+
+    # Create adjacency matrix
+    A_G = np.zeros((len(V_G), len(V_G)), dtype=int)
+    A_G[E_G[:,0], E_G[:,1]] = 1
+    A_G[E_G[:,1], E_G[:,0]] = 1
+
+    # Sample size
+    N = math.ceil((math.log(2) - math.log(1 - gamma)) / (2*epsilon**2))
+
+    # Sample vertex mappings V_g -> V_G
+    mappings = np.random.randint(low=0, high=len(V_G)-1, size=(N, len(V_g)))
+
+    # Create mapped vertex edges to later check in adjacency matrix
+    mapped_edges = np.empty(shape=(len(E_g)*N, 2), dtype=np.uint16)
+    for i in range(N):
+        mapped_edges[i*len(E_g):(i+1)*len(E_g)] = mappings[i][E_g]
+
+    # Get adjacency for each edge of the random mappings
+    adjacency_indicators = A_G[mapped_edges[:,0], mapped_edges[:,1]]
+
+    # Multiply adjacencies to check if the mappings represent homomorphisms then average
+    hom_density = (
+        adjacency_indicators
+        .reshape(len(E_g), N)
+        .prod(axis=0)
+        .sum()
+        / N
+    )
+
+    return hom_density
+
+
+def complete_graph(n):
+    V = np.arange(n)
+    E = np.transpose(np.vstack(np.tril_indices(n)))
+    E = E[E[:,0] != E[:,1]]
+
+    return V, E
+
+
+def path_graph(n):
+    V = np.arange(n)
+    E = []
+    for i in range(n-1):
+        E.append([i, i+1])
+    E = np.array(E)
+
+    return V, E
+
+
+def cycle_graph(n):
+    V = np.arange(n)
+    E = []
+    for i in range(n-1):
+        E.append([i, i+1])
+    E.append([n-1, 0])
+    E = np.array(E)
+
+    return V, E
+
+
+def star_graph(n):
+    V = np.arange(n+1)
+    E = []
+    for i in range(1, n+1):
+        E.append([0, i])
+    E = np.array(E)
+
+    return V, E
+
+
+def np_to_nx(G):
+    """Converts numpy representation of graph to networkx"""
+    nx_G = nx.Graph()
+    nx_G.add_nodes_from(G[0])
+    nx_G.add_edges_from(G[1])
+    return nx_G
+
+
+def nx_to_np(G):
+    """Converts networkx representation of graph to numpy arrays"""
+    V = np.array(G.nodes).astype(int)
+    E = nx.convert_matrix.to_pandas_edgelist(G).values.astype(int)
+    return V, E
+
+
+def create_all_graphs(n):
+    """Creates ALL possible isomorphism-unique graphs of size 2 to n"""
+    g = []
+    # Iterate over vertex counts v
+    for v in range(2, n+1):
+        K = complete_graph(v)
+        # Iterate over all possible graphs of size v
+        g_v = []
+        edge_selectors = np.array([i for i in product(range(2), repeat=int(v*(v-1)/2))], dtype=bool)
+        for es in edge_selectors:
+            # Discard if independent set
+            if es.sum() == 0:
+                continue
+            G = (K[0].copy(), K[1][es ,:].copy())
+            nx_G = np_to_nx(G)
+            # Check if new graph is isomorphic with any of the existing ones
+            isomorphic = False
+            for i in range(len(g_v)):
+                if nx.algorithms.isomorphism.is_isomorphic(g_v[i], nx_G):
+                    isomorphic = True
+                    break
+
+            if not isomorphic:
+                g_v.append(nx_G)
+        g = g + g_v
+    # Convert networkx graph representation to numpy arrays
+    g = [nx_to_np(G) for G in g]
+
+    return g
