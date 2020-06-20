fe117be00273805c11979c16aabd8e7320145a32
Huy Hoang Vu <honzahoang@gmail.com>
2020-06-20 17:28:04 +0200

    eod

--- nngraphons/learning/gradient_learning.py
+++ nngraphons/learning/gradient_learning.py
@@ -27,7 +27,7 @@ def gradient_descent(
         List of small graphs for which to match homomorphism densities of the
         network and training graph
     net : torch.nn.Module
-        PyTorch neural netowrk to represent graphon
+        PyTorch neural network to represent graphon
     n : int
         Number of samples to use for density approximation at each training iteration
     t_g : List[float]
@@ -70,3 +70,39 @@ def gradient_descent(
 
         # Update weights
         optimizer.step()
+
+
+def multiple_gradient_descent_algorithm(
+    g: List[Tuple],
+    net: nn.Module,
+    n: int,
+    t_g: List[float],
+    losses: List[Callable[[List[float], List[torch.Tensor]], float]],
+    stopping_criterion: Callable[[float], bool],
+    optimizer: torch.optim.Optimizer
+) -> None:
+    torch.set_grad_enabled(False)
+
+    # Training loop
+    while True:
+        # Calculate homomorphism densities w.r.t network
+        t_net = [t_nn(F, net, n, True) for F in g]
+
+        # For each loss / objective...
+        for L in losses:
+            # Zero out old gradients
+            net.zero_grad()
+            # Compute loss value
+            loss = L(t_g, t_net)
+            # Compute new gradients w.r.t computed loss value
+            loss.backward(retain_graph=True)
+            # Save loss specific gradient as 1D vector
+            loss_gradients = []
+            for p in net.parameters():
+                loss_gradients.append(torch.new_tensor(p))
+
+
+        # Update weights
+        optimizer.step()
+        # Get gradients as vectors
+
