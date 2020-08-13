884dfd7ac3d0bc3cd09ad4ee6204d45644acff5a
Huy Hoang Vu <honzahoang@gmail.com>
2020-08-13 16:40:59 +0200

    net sampling

--- nngraphons/data_loading/synthetic_graphs.py
+++ nngraphons/data_loading/synthetic_graphs.py
@@ -1,7 +1,9 @@
 from itertools import product
+import os
 import random
 import numpy as np
 import networkx as nx
+import torch
 
 
 def graphon_grow_unit_attach(x, y):
@@ -43,6 +45,38 @@ def sample_graphon(W, size=None, max_size=None):
     edges = V[edge_indices]
     # Edge probabilites (graphon values)
     edge_probs = W(edges[:, 0], edges[:, 1])
+
+    # Convert float values of vertices to integer indexes
+    V = np.arange(len(V))
+
+    # Threshold probabilites to decide which pairs become edges
+    E = np.random.uniform(size=len(edge_probs)) <= edge_probs
+    E = edge_indices[E]
+
+    return V, E
+
+
+def sample_net(net, size=None, max_size=None):
+    """Returns a random graph sampled from network net."""
+    # Number of vertices
+    n_V = random.randint(2, max_size) if size is None else size
+    # Randomly select nodes in graphon
+    V = np.random.uniform(size=n_V)
+    # All possible edges
+    edge_indices = np.transpose(np.vstack(np.tril_indices(len(V))))
+    edges = V[edge_indices]
+    # Edge probabilites (graphon values)
+    with torch.no_grad():
+        edge_probs = (
+            net(
+                torch
+                .from_numpy(edges)
+                .float()
+                .to(os.environ['COMPUTATION_DEVICE'])
+            )
+            .cpu()
+            .numpy()
+        )
 
     # Convert float values of vertices to integer indexes
     V = np.arange(len(V))
